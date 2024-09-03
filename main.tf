217f3fb953580417fcbf4ca9cf5ecc9bc0dcce71
Felipe <felipe@MacBook-Pro-de-Felipe.local>
2024-09-02 21:11:57 -0300

    init: initial commit instance config

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-05134c8ef96964280"
+  instance_type = "t2.micro"
+  key_name      = "iac"
+
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
